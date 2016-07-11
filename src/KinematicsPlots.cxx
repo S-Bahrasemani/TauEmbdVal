d4ed3c9b1fdff871bf8c6e7316767fe3a6fc367d
Hamze Baluch Bahrasemani <hbaluchb@lxplus030.cern.ch>
2016-07-11 22:08:32 +0200

    update binning

--- src/KinematicsPlots.cxx
+++ src/KinematicsPlots.cxx
@@ -10,19 +10,19 @@ namespace TauEmbd{
 void KinematicsPlots::initializePlots(){
   lead = nullptr;
   npart = 0;
-
-   n  = Book1D("n", "Number of "+ m_sParticleType +"; n ;Events", 50, 0., 50);
-
-   et  = Book1D("et", "E_{T} of "+ m_sParticleType +"; E_{T} (GeV) ;Events", 100, 0., 1000);
-   eta = Book1D("eta", "#eta of " + m_sParticleType +";#eta; Events ", 20,-10.,10.);
-   phi = Book1D("phi", "#varphi of " + m_sParticleType +";#varphi;Events ",10 ,-3.2,3.2);
-
-   lead_et  = Book1D("lead_et", "E_{T} of lead "+ m_sParticleType +"; E_{T} (GeV) ;Events", 100, 0., 1000);
-   lead_eta = Book1D("lead_eta", "#eta of lead " + m_sParticleType +";#eta; Events ", 20,-10.,10.);
-   lead_phi = Book1D("lead_phi", "#varphi of lead " + m_sParticleType +";#varphi;Events ",10 ,-3.2,3.2);
-}
-
-void KinematicsPlots::fill(const xAOD::IParticle* part){
+  
+  n  = Book1D("n", "Number of "+ m_sParticleType +"; n ;Events", 30, 0., 30);
+    
+  et  = Book1D("et", "E_{T} of "+ m_sParticleType +"; E_{T} (GeV) ;Events", 30, 0., 300);
+  eta = Book1D("eta", "#eta of " + m_sParticleType +";#eta; Events ",  20,-10.,10.);
+  phi = Book1D("phi", "#varphi of " + m_sParticleType +";#varphi;Events ",10 ,-4,4);
+    
+  lead_et  = Book1D("lead_et", "E_{T} of lead "+ m_sParticleType +"; E_{T} (GeV) ;Events", 30, 0., 300);
+  lead_eta = Book1D("lead_eta", "#eta of lead " + m_sParticleType +";#eta; Events ", 20,-10.,10.);
+  lead_phi = Book1D("lead_phi", "#varphi of lead " + m_sParticleType +";#varphi;Events ",10 ,-4,4);
+    }
+  
+  void KinematicsPlots::fill(const xAOD::IParticle* part){
 
   et->Fill(part->pt()/GeV);
   eta->Fill(part->eta());
